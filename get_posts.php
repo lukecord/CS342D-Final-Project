dc7e3e1b2e65763e10bae4105a1e27f6f4fca644
root <lukecdr@outlook.com>
2025-04-25 03:41:11 +0000

    Got basic functionality working with current DB structure. Utilizes service user to protect from vulnerabilities.

--- get_posts.php
+++ get_posts.php
@@ -1,12 +1,12 @@
 <?php
 include 'db_connect.php';
 
-$sql = "SELECT * FROM BlogPosts";
+$sql = "SELECT * FROM Posts;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["PostID"]. " - Title: " . $row["Title"]. " " . $row["Content"]. "<br>";
+        echo "id: " . $row["PostID"]. " - Title: " . $row["Title"]. " - Content: " . $row["Content"]. "<br>";
     }
 } else {
     echo "0 results";
--- login.php
+++ login.php
@@ -4,16 +4,21 @@ include 'db_connect.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$sql = "SELECT * FROM Users WHERE Username='$username' AND Password='$password'";
+$sql = "SELECT * FROM Users WHERE Username='$username' AND Passwrd='$password';";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Login Successful
     echo "Login Successful";
     // Set session variables or redirect to another page
+    session_start();
+    $_SESSION['username'] = $username;
+    header("Location: get_posts.html");
+    exit;
 } else {
     // Login Failed
     echo "Invalid Username or Password";
 }
+
 $conn->close();
 ?>
